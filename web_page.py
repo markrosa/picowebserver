6ac1e3f9e6fc882a51ea3a2365b876f8e45115f8
Mark Rosa <markrosa@users.noreply.github.com>
2023-07-25 19:58:15 +0200

    login-page entfernt; erste buttons und routen aktiviert

--- web_page.py
+++ web_page.py
@@ -12,53 +12,50 @@ led = Pin("LED", Pin.OUT)
 led.off()
 mode_text = "OFF"
 
-
-#     if request_url.find("/ledon") != -1:
-#         # LED anschalten
-#         mode_text = "LED ON"
-#         led.on()
-#     if request_url.find("/ledoff") != -1:
-#         # LED ausschalten
-#         mode_text = "LED OFF"
-#         led.off()
-#     if request_url.find("/lauflicht") != -1:
-#         # Lauflicht starten
-#         mode_text = "Lauflicht"
-#         audi_blinker()
-    
-# wir lesen den Quelltext der HTML Seite ein
-html = html_page
-
-# das Template "**MODE**" im HTML-Quelltext wird ersetzt
-html = html.replace('**MODE**', mode_text)
-
 @server.route("/")
 def index(request):
-    return render_template("index.html", name="Mark", title="Mark's Personal Blog", content="This is the content")
+    return render_template("index.html", name="Mark Rosa", title="Lichterkettensteuerung", MODE=mode_text)
 
 
 @server.route("/ledon")
 def index(request):
-    return render_template("index.html", name="Mark", title="Mark's Personal Blog", content=html)
+    # LED anschalten
+    mode_text = "LED ON"
+    led.on()
+    return render_template("index.html", name="Mark Rosa", title="Lichterkettensteuerung", MODE=mode_text)
+
+@server.route("/ledoff")
+def index(request):
+    # LED ausschalten
+    mode_text = "LED OFF"
+    led.off()
+    return render_template("index.html", name="Mark Rosa", title="Lichterkettensteuerung", MODE=mode_text)
+
+@server.route("/lauflicht")
+def index(request):
+    # Lauflicht
+    mode_text = "Lauflicht"
+    audi_blinker()
+    return render_template("index.html", name="Mark Rosa", title="Lichterkettensteuerung", MODE=mode_text)
 
 
 @server.route("/about")
 def index(request):
-    return render_template("about.html", name="Mark", title="About this site")
+    return render_template("about.html", name="Mark Rosa", title="About this site")
 
-@server.route("/login", ['POST', 'GET'])
-def login_form(request):
-    print(request.method)
-    if request.method == 'GET':
-        return render_template("login.html")
-    if request.method == 'POST':
-        username = request.form.get("username", None)
-        password = request.form.get("password", None)
-        
-        if username == "rosa" and password == "password":
-            return render_template('index.html', title="Welcome", content = f"<h1 class=\"display-5\">Welcome back, {username}</h1>")
-        else:
-            return render_template('index.html', title="Not logged in", content = "Invalid username or password")
+# @server.route("/login", ['POST', 'GET'])
+# def login_form(request):
+#     print(request.method)
+#     if request.method == 'GET':
+#         return render_template("login.html")
+#     if request.method == 'POST':
+#         username = request.form.get("username", None)
+#         password = request.form.get("password", None)
+#         
+#         if username == "rosa" and password == "password":
+#             return render_template('index.html', title="Welcome", content = f"<h1 class=\"display-5\">Welcome back, {username}</h1>")
+#         else:
+#             return render_template('index.html', title="Not logged in", content = "Invalid username or password")
 
 @server.catchall()
 def my_catchall(request):
